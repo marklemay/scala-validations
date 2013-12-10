b1b8b01c0d42171788e0dd4c9554d8863015cd5f
marklemay <riteofwhey@gmail.com>
2013-12-09 23:05:55 -0500

    Create RegexTest.scala

---
+++ examples/macroTest/src/macroTest/RegexTest.scala
@@ -0,0 +1,12 @@
+package macroTest
+
+object RegexTest {
+
+  def main(args: Array[String]): Unit = {
+    import riteofwhey.ocd.regex.RegexValidator._
+
+    val reg = r"aaaaa[dd]d"
+    
+  }
+
+}
